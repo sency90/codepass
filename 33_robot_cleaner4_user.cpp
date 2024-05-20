51435c12ab62c3d2d60def306245215866bd590a
plzrun <sency90@gmail.com>
2024-05-20 23:32:36 +0900

    update

--- 33_robot_cleaner4_user.cpp
+++ 33_robot_cleaner4_user.cpp
@@ -6,54 +6,91 @@ extern void turn(int mCommand);
 int M;
 int fs[3][3];
 int m[131][131];
+bool chk[131][131];
+int dx[4] = {-1,0,1,0};
+int dy[4] = {0,-1,0,1};
+int dx2[4] = {1,-1,-1,1}; //right back
+int dy2[4] = {1,1,-1,-1}; //right back
+
 void init(void) {
 	for(int i=0; i<131; i++) {
 		for(int j=0; j<131; j++) {
 			m[i][j] = -1;
+			chk[i][j] = false;
 		}
 	}
 }
 
 bool has_been(int rx, int ry, int dir) {
-	for(int dir=0; dir<4; dir++) {
-		switch(dir) {
-			case 0:
-				for(int i=rx-1; i<=rx+1; i++) {
-					for(int j=ry-1; j<=ry+1; j++) {
-						if(m[i][j]<0) return false;
-					}
-				}
-				break;
-			case 1:
-				for(int i=ry-1; i<=ry+1; i++) {
-					for(int j=rx+1; j>=rx-1; j--) {
-						if(m[j][i]<0) return false;
-					}
-				}
-				break;
-			case 2:
-				for(int i=rx+1; i>=rx-1; i--) {
-					for(int j=ry+1; j>=ry-1; j--) {
-						if(m[i][j]<0) return false;
-					}
-				}
-				break;
-			case 3:
-				for(int i=ry+1; i>=ry-1; i--) {
-					for(int j=rx-1; j<=rx+1; j++) {
-						if(m[j][i]>0) return false;
-					}
-				}
-				break;
-			default:
-				break;
-		}
+	switch(dir) {
+		case 0:
+			for(int i=rx-1; i<=rx+1; i++) 
+				for(int j=ry-1; j<=ry+1; j++) 
+					if(m[i][j]<0) return false;
+			break;
+		case 1:
+			for(int j=ry-1; j<=ry+1; j++) 
+				for(int i=rx+1; i>=rx-1; i--) 
+					if(m[i][j]<0) return false;
+			break;
+		case 2:
+			for(int i=rx+1; i>=rx-1; i--) 
+				for(int j=ry+1; j>=ry-1; j--) 
+					if(m[i][j]<0) return false;
+			break;
+		case 3:
+			for(int j=ry+1; j>=ry-1; j--) 
+				for(int i=rx-1; i<=rx+1; i++) 
+					if(m[i][j]<0) return false;
+			break;
 	}
 	return true;
 }
 
-int next_x(int dir) {
-	
+void draw_map(int (*fs)[3], int rx, int ry, int rdir) {
+	switch(rdir) {
+		case 0:
+			for(int i=0,u=rx-1; i<3; i++,u++) 
+				for(int j=0,v=ry-1; j<3; j++,v++) 
+					m[u][v] = fs[i][j];
+			break;
+		case 1:
+			for(int i=0,v=ry-1; i<3; i++,v++) 
+				for(int j=0,u=rx+1; j<3; j++,u--) 
+					m[u][v] = fs[i][j];
+			break;
+		case 2:
+			for(int i=0,u=rx+1; i<3; i++,u--) 
+				for(int j=0,v=ry+1; j<3; j++,v--) 
+					m[u][v] = fs[i][j];
+			break;
+		case 3:
+			for(int i=0,v=ry+1; i<3; i++,v--) 
+				for(int j=0,u=rx-1; j<3; j++,u++) 
+					m[u][v] = fs[i][j];
+			break;
+	}
+
+}
+
+struct Coord{
+	int x,y;
+	Coord() {}
+	Coord(int x, int y):x(x),y(y) {}
+	bool operator<(const Coord & rhs) const {
+		if(x == rhs.x) return y<rhs.y;
+		else return x<rhs.x;
+	}
+
+};
+
+int next_dir(int rx, int ry, int rdir) {
+	int front_x=rx+dx[rdir], front_y=ry+dy[rdir];
+	if(m[front_x][front_y]==1) { //전진 방향이 벽이면,
+		int left_dir = (rdir+1)%4;
+		return next_dir(rx, ry, left_dir);
+	}
+	else return rdir;
 }
 
 
@@ -62,11 +99,29 @@ void cleanHouse(void)
 	int rx=64, ry=64, rdir=0;
 
 	while(true) {
+		chk[rx][ry] = true;
 		if(!has_been(rx,ry,rdir)) {
 			scan(fs);
+			draw_map(fs,rx,ry,rdir);
 		}
 
-		int nx = next_x(rdir), ny = next_y(rdir);
+		//벽타기 Logic
+		int right_dir = (rdir+3)%4;
+		int right_x=rx+dx[right_dir], right_y=ry+dy[right_dir];
+		if(m[right_x][right_y]==1) { //오른쪽이 벽이면,
+			//전진할건데, 그 자리가 벽인지 확인. 왼쪽으로 돌면서 다음으로 갈 수 있는 곳을 search
+			int nrdir = next_dir(rx,ry,rdir);
+			if(nrdir != rdir) {
+				turn((nrdir+4 - rdir)%4); //방향 전환
+				rdir = nrdir;
+			}
+			move();
+			rx += dx[rdir];
+			ry += dy[rdir];
+		}
+		else {
+			int right_back_x = rx+dx2[rdir], right_back_y = ry+dy2[rdir];
+		}
 	}
 
 
--- test2.cpp
+++ test2.cpp
@@ -1,25 +1,58 @@
-#include <stdio.h>
-#include <string.h>
-struct Info{
-	int x[128];
-	Info() {
-		memset(x, 0, sizeof(x));
+#include <cstdio>
+#include <cstring>
+using namespace std;
+typedef unsigned int uint;
+int fs[3][3];
+int m[9][9];
+uint seed = 31;
+uint pseudo_rand() {
+	return seed = (seed * 511) ^ seed;
+}
+int main() {
+	int cnt=0;
+	for(int i=0; i<3; i++) {
+		for(int j=0; j<3; j++) {
+			fs[i][j] = ++cnt;
+		}
 	}
-	void print() const {
-		for(int i=0; i<128; i++) {
-			printf("%d ", x[i]);
+
+	int rx=3, ry=5;
+
+	for(int rdir=0; rdir<4; rdir++) {
+		int rx = pseudo_rand()%7+1, ry= pseudo_rand()%7+1;
+		printf("(rx,ry) = (%d,%d)\n", rx,ry);
+		memset(m, 0, sizeof(m));
+		switch(rdir) {
+			case 0:
+				for(int i=0,u=rx-1; i<3; i++,u++) 
+					for(int j=0,v=ry-1; j<3; j++,v++) 
+						m[u][v] = fs[i][j];
+				break;
+			case 1:
+				for(int i=0,v=ry-1; i<3; i++,v++) 
+					for(int j=0,u=rx+1; j<3; j++,u--) 
+						m[u][v] = fs[i][j];
+				break;
+			case 2:
+				for(int i=0,u=rx+1; i<3; i++,u--) 
+					for(int j=0,v=ry+1; j<3; j++,v--) 
+						m[u][v] = fs[i][j];
+				break;
+			case 3:
+				for(int i=0,v=ry+1; i<3; i++,v--) 
+					for(int j=0,u=rx-1; j<3; j++,u++) 
+						m[u][v] = fs[i][j];
+				break;
+		}
+
+		for(int i=0; i<9; i++) {
+			for(int j=0; j<9; j++) {
+				if(m[i][j]==0) printf(".");
+				else printf("%d",m[i][j]);
+			}
+			puts("");
 		}
 		puts("");
 	}
-};
-int main() {
-	Info info[2];
-	for(int i=0; i<128; i++) {
-		info[0].x[i] = i;
-	}
-
-	info[1].print(); puts("");
-	info[1] = info[0];
-	info[1].print();
 	return 0;
 }
